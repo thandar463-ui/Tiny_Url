aa5a422ecbfcfbab89e9af2d085390621a44cc89
Win Thandar Aung <aungthandar463@gmail.com>
2026-07-14 09:32:04 +0630

    Generate auth guard, service, module and controller api etc

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,4 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
-@Module({})
-export class PrismaModule {}
+
+@Global()
+@Module({
+    providers: [
+        PrismaService,
+    ],
+    exports: [
+        PrismaService,
+    ],
+})
+export class PrismaModule { }
